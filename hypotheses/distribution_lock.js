206a31a283f82e362a6e5d499957f81a93fcd9d0
shiningdong <yd262@cornell.edu>
2017-12-12 00:26:43 -0500

    added distribution_lock.js

--- hypotheses/distribution_lock.js
+++ hypotheses/distribution_lock.js
@@ -1,5 +1,20 @@
 'use strict'
 
-module.exports = () => {
+const threshold = 0.58
+const _ = require('lodash');
 
+
+// history: [3, 2, 6]
+// results: [{ans: 'A', index: 0, count: 5}, {ans: 'B', index: 1, count: 3}, {ans: 'C', index: 2, count: 1}]
+module.exports = (total, hitstory, results) => {
+	const limit = Math.floor(total * threshold);
+
+	_.each(results, (choice, index) => {
+		results[index].recommend = false;
+		if(history[index] + 1 <= limit) {
+			results[index].recommend = true;
+			return false;
+		}
+	});
+	return results;
 };

5eacfde8a4327cbeb3702f34810b7c0a11e4cbf2
Chris Young <chris.young@gotinder.com>
2017-12-13 13:06:01 -0800

    Cache google searches

--- common.js
+++ common.js
@@ -1,7 +1,10 @@
 'use strict'
 
+const fs = require('fs');
 const VError = require('verror');
 const request = require('request');
+
+const cache = require('./data/cache.json');
 
 const DEBUG = process.env.HQ_DEBUG;
 
@@ -15,13 +18,17 @@ const COLORS = {
 };
 
 const out = (m, c, s, o) => o ? console[m](c, s, o, '\x1b[0m') : console[m](c, s, '\x1b[0m');
-
-exports.log = (msg, data) => out('log', COLORS.WHITE, msg, data);
-exports.exit = (code, msg, data) => out('error', COLORS.RED, msg, data) || process.exit(code);
-exports.debug = (msg, data) => void (DEBUG && out('log', COLORS.GREEN, `DEBUG: ${msg}`, data));
-exports.warn = (msg, data) => out('error', COLORS.YELLOW, `WARN: ${msg}`, data);
+const log = (msg, data) => out('log', COLORS.WHITE, msg, data);
+const exit = (code, msg, data) => out('error', COLORS.RED, msg, data) || process.exit(code);
+const debug = (msg, data) => void (DEBUG && out('log', COLORS.GREEN, `DEBUG: ${msg}`, data));
+const warn = (msg, data) => out('error', COLORS.YELLOW, `WARN: ${msg}`, data);
 
 Object.keys(COLORS).forEach((key) => exports.log[key.toLowerCase()] = (msg, data) => out('log', COLORS[key], msg, data));
+
+exports.log = log;
+exports.exit = exit;
+exports.debug = debug;
+exports.warn = warn;
 
 exports.googleSearch = (query, cb) => {
 	const opts = {
@@ -29,12 +36,19 @@ exports.googleSearch = (query, cb) => {
 		url: `https://www.google.com/search?q=${encodeURIComponent(query)}`
 	};
 
+	if (cache[opts.url])
+		return setImmediate(() => cb(null, cache[opts.url]));
+
 	request(opts, (err, res, body) => {
 		if (err)
 			return cb(new VError(err, 'Failed to make request'), null);
 
 		if (res.statusCode !== 200)
 			return cb(new VError(`Got bad status code from google: ${res.statusCode}`), { headers: res.headers, body });
+
+		cache[opts.url] = body;
+
+		fs.writeFile('./data/cache.json', JSON.stringify(cache), (err) => err && warn('Failed to update cache', err));
 
 		cb(null, body);
 	});
@@ -46,12 +60,19 @@ exports.wikiSearch = (query, cb) => {
 		url: `https://en.wikipedia.org/wiki/Special:Search?search=${encodeURIComponent(query)}`,
 	};
 
+	if (cache[opts.url])
+		return setImmediate(() => cb(null, cache[opts.url]));
+
 	request(opts, (err, res, body) => {
 		if (err)
 			return cb(new VError(err, 'Failed to make request'), null);
 
 		if (res.statusCode !== 200)
 			return cb(new VError(`Got bad status code from wiki: ${res.statusCode}`), { headers: res.headers, body });
+
+		cache[opts.url] = body;
+
+		fs.writeFile('./data/cache.json', JSON.stringify(cache), (err) => err && warn('Failed to update cache', err));
 
 		cb(null, body);
 	});

fa3d1448693cb2c8902e1d9af1285f6b628e2485
Chris Young <chris.young@gotinder.com>
2017-12-13 14:34:28 -0800

    Remove async

--- tools/check_accuracy.js
+++ tools/check_accuracy.js
@@ -1,45 +1,47 @@
 'use strict'
 
 const fs = require('fs');
-const async = require('async');
 
 const { exit, warn, log } = require('../common');
 const { questionSearch } = require('../hypotheses');
 
+const BROADCAST_ID = process.argv[2];
+
 let files = fs.readdirSync('./data/questions');
 
-if (process.argv[2])
-	files = files.filter((file) => file === `${process.argv[2]}.json`);
+if (BROADCAST_ID)
+	files = files.filter((file) => file === `${BROADCAST_ID}.json`);
 
 if (!files.length)
-	exit(1, `Failed to find question data for broadcast: ${process.argv[2]}`);
+	exit(1, `Failed to find question data for broadcast: ${BROADCAST_ID}`);
 
-async.parallel(files.map((file) => (cb) => {
+const tests = files.map((file) => {
 	const questions = require(`../data/questions/${file}`);
 	const summaries = require(`../data/summaries/${file}`);
 
-	const tasks = questions.map((question, index) => (cb) => {
+	const tasks = questions.map((question, index) => new Promise((resolve, reject) => {
 		const summary = summaries[index];
 
 		questionSearch(question, (err, answers) => {
 			if (err)
-				return cb(err);
+				return reject(err);
 
 			answers[0].correct = summary.answerCounts[answers[0].index].correct;
 
-			cb(null, answers[0]);
+			resolve(answers[0]);
 		});
-	});
+	}));
 
-	async.parallel(tasks, cb);
-}), (err, questions) => {
-	if (err)
-		return warn({ err });
-
-	let sum = 0;
-	let correct = 0;
-
-	questions.forEach((guesses) => guesses.forEach((result) => ++sum && (result.correct && ++correct)));
-
-	log({ accuracy: correct / sum });
+	return Promise.all(tasks);
 });
+
+Promise.all(tests)
+	.then((questions) => {
+		let sum = 0;
+		let correct = 0;
+
+		questions.forEach((guesses) => guesses.forEach((result) => ++sum && (result.correct && ++correct)));
+
+		log({ accuracy: correct / sum });
+	})
+	.catch((err) => warn(err));
